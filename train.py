50dd563854cb35eb5c405778a29d2374a9139493
Darish05 <153887116+Darish05@users.noreply.github.com>
2025-11-13 08:40:34 +0530

    change

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrices import accuracy_score
+from sklearn.metrics import accuracy_score
 import joblib
 
 data=pd.read_csv('iris.csv')
